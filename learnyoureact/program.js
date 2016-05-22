034da0c93778abe140bdf12830f4eab8202c45ab
Alexio <alexioamota@gmail.com>
2016-05-22 15:51:14 -0400

    Learnyoureact exercise 7

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -13,8 +13,22 @@ require('babel/register')({
 	ignore: false
 });
 
+var firstDetail = process.argv[3] || "Shopping things";
+var secondDetail = process.argv[4] || "1:00 PM";
+var data = [
+	{
+		title: "Shopping",
+		detail: firstDetail
+	},
+	{
+		title: "Hair cut",
+		detail: secondDetail
+	}
+];
 app.use('/', function(req, res) {
-	res.render('index', '');
+	res.render('index', {
+		data: data
+	});
 });
 
 app.listen(app.get('port'), function() {});
